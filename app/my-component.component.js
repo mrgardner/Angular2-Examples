c58f786821588d2b99a9230493ac474413959b1c
Matthew Gardner <mgardner@kenzanmedia.com>
2016-06-30 14:02:31 -0400

    Updating component intro example branch

---
+++ app/my-component.component.js
@@ -0,0 +1,38 @@
+System.register(['@angular/core'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var MyComponentComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            MyComponentComponent = (function () {
+                function MyComponentComponent() {
+                }
+                MyComponentComponent = __decorate([
+                    core_1.Component({
+                        selector: 'my-component',
+                        template: "\n        This is my component!\n   "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], MyComponentComponent);
+                return MyComponentComponent;
+            }());
+            exports_1("MyComponentComponent", MyComponentComponent);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm15LWNvbXBvbmVudC5jb21wb25lbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7WUFRQTtnQkFBQTtnQkFFQSxDQUFDO2dCQVJEO29CQUFDLGdCQUFTLENBQUM7d0JBQ1AsUUFBUSxFQUFFLGNBQWM7d0JBQ3hCLFFBQVEsRUFBRSxzQ0FFVjtxQkFDSCxDQUFDOzt3Q0FBQTtnQkFHRiwyQkFBQztZQUFELENBRkEsQUFFQyxJQUFBO1lBRkQsdURBRUMsQ0FBQSIsImZpbGUiOiJteS1jb21wb25lbnQuY29tcG9uZW50LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHtDb21wb25lbnR9IGZyb20gJ0Bhbmd1bGFyL2NvcmUnO1xuXG5AQ29tcG9uZW50KHtcbiAgICBzZWxlY3RvcjogJ215LWNvbXBvbmVudCcsXG4gICAgdGVtcGxhdGU6IGBcbiAgICAgICAgVGhpcyBpcyBteSBjb21wb25lbnQhXG4gICBgXG59KVxuZXhwb3J0IGNsYXNzIE15Q29tcG9uZW50Q29tcG9uZW50IHtcbiAgICBcbn0iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
+import {MyComponentComponent} from './my-component.component';
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!!</p>
-       
+        <h1>This is a test</h1>
+        <h2>Now comes the second component</h2>
+        <my-component></my-component>    
     `,
+    directives: [MyComponentComponent]
 })
 export class AppComponent {
 
