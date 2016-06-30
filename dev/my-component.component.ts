5400293b3f0ac3e6563ab03bd6551aa60f8a0b4f
Matthew Gardner <mgardner@kenzanmedia.com>
2016-06-30 13:55:03 -0400

    Updating master

---
+++ dev/my-component.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-component',
+    template: `
+        This is my component!
+   `
+})
+export class MyComponentComponent {
+    
+}
