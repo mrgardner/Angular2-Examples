4eaf4933626c64ae2fd6c02c9f512e6e4e11ef1b
Matthew Gardner <mgardner@kenzanmedia.com>
2016-07-05 15:04:38 -0400

    Updating pipes example

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,11 +3,45 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!!</p>
+        <section class="pipe">
+            <h2>The Date Pipe</h2>
+            <p>Today is: {{today | date:'short'}}</p>
+        </section>
+        
+        <section class="pipe">
+            <h2>The lowercase and UPPERCASE Pipe</h2>
+            <input type="text" #inputCasePipe (keyup)="0"><br>
+            <div>Output Lowercase {{inputCasePipe.value | lowercase}}</div>
+            <div>Output Uppercase {{inputCasePipe.value | uppercase}}</div>
+        </section>
+        
+        <section class="pipe">
+            <h2>The Slice Pipe</h2>
+            <input type="text" #inputSlicePipe (keyup)="0"> - from <input type="text" #start (keyup)="0"> to <input type="text" #end (keyup)="0">
+            <div>Output: {{inputSlicePipe.value | slice:start.value:end.value}}</div>
+        </section>
+        
+        <section class="pipe">
+            <h2>Number Pipes</h2>
+            <input type="text" #inputNumberPipes (keyup)="0"> - currency: <input type="text" #currency value="EUR" (change)="0"><br>
+            <div>Decimal: {{1.0 * inputNumberPipes.value | number:'1.1-2'}}</div>
+            <div>Currency: {{1.0 * inputNumberPipes.value | currency:currency.value:currencyShort.checked:'1.2-2' }}</div>
+            <input type="checkbox" #currencyShort checked (change)="0">Short currency code  
+        </section>
+        
+        <section class="pipe">
+            <h2>Chaining multiple Pipes (e.g. uppercase and slice</h2>
+            <input type="text" #inputChainPipes (keyup)="0"><br>
+            <div>Output: {{inputChainPipes.value | slice:1:3 | uppercase}}</div>
+        </section>
+        
+        <section class="pipe">
+            
+        </section>
+        
        
     `,
 })
 export class AppComponent {
-
+    today = new Date();
 }
